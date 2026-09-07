59969e2fb3a71bf42a9e67d70e18f82fd5dd453c
Pavithra K <pavithrakannan010@gmail.com>
2026-09-07 22:15:02 +0530

    Connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://ecommerce-pavithra-k.onrender.com";
 
 function App() {
   const [products, setProducts] = useState([]);
